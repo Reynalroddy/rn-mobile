a67dfe310a0249a12fa6b2de619686d221c900eb
Ajetunmobi Isaac <isaacajetunmobi@gmail.com>
2023-07-05 00:43:38 +0100

    fixed stack issues

--- navs/AppNav.js
+++ navs/AppNav.js
@@ -1,42 +1,34 @@
-import { View, Text } from 'react-native'
-import React ,{useState,useCallback,useEffect}from 'react'
+import {View, Text} from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
 import Main from './Main';
 import Auth from './Auth';
-import { useSelector,useDispatch } from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
+import {NavigationContainer} from '@react-navigation/native';
 
-import { NavigationContainer } from '@react-navigation/native';
-
-import { Toast } from 'react-native-toast-message/lib/src/Toast';
+import {Toast} from 'react-native-toast-message/lib/src/Toast';
 // import { setTokenInterceptor } from './utils/supporters';
 // SplashScreen.preventAutoHideAsync();
 const AppNav = () => {
-    const { user,loading,token } = useSelector((state) => state.user);
-    // console.log(user);
+  const {user, loading, token} = useSelector(state => state.user);
+  // console.log(user);
   //  const user= false;
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //       if(!user) return;
+  //       setTokenInterceptor(token)
 
-// useEffect(() => {
-  
-//       if(!user) return;
-//       setTokenInterceptor(token)
-   
-// }, []);
-
+  // }, []);
 
   return (
-    <View  style={{ flex: 1 }}>
-            <NavigationContainer>
-   {
-!user?
-      <Auth/> : 
-<Main/>
-   }
-     <Toast position='top' topOffset={40}/>
-   </NavigationContainer>
-</View>
-  )
-}
+    <View style={{flex: 1}}>
+      <NavigationContainer>
+        {!user ? <Auth /> : <Main />}
+        <Toast position="top" topOffset={40} />
+      </NavigationContainer>
+    </View>
+  );
+};
 
-export default AppNav
+export default AppNav;
--- navs/Main.js
+++ navs/Main.js
@@ -1,13 +1,16 @@
-
-import React,{useEffect} from 'react'
-import { UserIcon,ShoppingCartIcon,HomeIcon } from "react-native-heroicons/solid";
-import { useSelector } from 'react-redux';
+import React, {useEffect} from 'react';
+import {
+  UserIcon,
+  ShoppingCartIcon,
+  HomeIcon,
+} from 'react-native-heroicons/solid';
+import {useSelector} from 'react-redux';
 import Cart from '../screens/Cart';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import CustomDrawer from '../components/CustomDrawer';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
 import Landing from '../screens/Landing';
@@ -23,108 +26,180 @@ import Product from '../screens/Product';
 import Checkout from '../screens/Checkout';
 import Pay from '../screens/Pay';
 import AdminOrders from '../screens/admin/Orders';
-const Main = () => {
+import {CommonActions, useNavigationState} from '@react-navigation/native';
 
-    // const Drawer = createNativeDrawerNavigator();
-    // const { isOnBoardingDisabled } = useSelector((state) => state.user);
-    const HomeStack = () => {
-      return (
-        <Stack.Navigator>
-            <Stack.Screen name="Landing" component={Landing}  options={{headerShown: false}} />
-             <Stack.Screen name="WorkOut" component={WorkOut}  options={{headerShown: false}} />
-             <Stack.Screen name="FitScreen" component={FitScreen}  options={{headerShown: false}} />
-             <Stack.Screen name="Rest" component={Rest}  options={{headerShown: false}} />
-        </Stack.Navigator>
-      );
-    };
-
-    const AdminStack=()=>{
-      return (
-        <Stack.Navigator>
-            <Stack.Screen name="MyAdmin" component={Admin}  options={{headerShown: false}} />
-             <Stack.Screen name="AdminOrders" component={AdminOrders}  options={{headerShown: false}} />
-             <Stack.Screen name="AdminProducts" component={Sub}  options={{headerShown: false}} />
-             {/* <Stack.Screen name="Admin" component={Admin}  options={{headerShown: false}} />
-             <Stack.Screen name="ChangePassword" component={ChangePassword}  options={{headerShown: false}} /> */}
-         
-        </Stack.Navigator>
-      )
-    }
-
-    
-       const ProfileStack = ({navigation}) => {
-        console.log('render')
-
-
-        useEffect(()=> {
-         return ()=> {
-          console.log('left')
-         }
-        }, []);
-        return (
-          <Stack.Navigator initialRouteName='MyProfile'>
-              <Stack.Screen name="MyProfile" component={Profile}  options={{headerShown: false}} />
-               <Stack.Screen name="Orders" component={Orders}  options={{headerShown: false}} />
-               <Stack.Screen name="Sub" component={Sub}  options={{headerShown: false}} />
-               <Stack.Screen name="Admin" component={AdminStack}  options={{headerShown: false}} />
-               <Stack.Screen name="ChangePassword" component={ChangePassword}  options={{headerShown: false}} />
-           
-          </Stack.Navigator>
-        );
-      };
-
-
-
-
-                     
-        
-
-      const ProductStack = () => {
-        return (
-          <Stack.Navigator>
-              <Stack.Screen name="MyProduct" component={Product}  options={{headerShown: false}} />
-            <Stack.Screen name="Cart" component={Cart}  options={{headerShown: false}} />
-                <Stack.Screen name="Checkout" component={Checkout}  options={{headerShown: false}} />
-                <Stack.Screen name="Pay" component={Pay}  options={{headerShown: false}} />
-           
-          </Stack.Navigator>
-        );
-      };
+const HomeStack = () => {
   return (
-      <Drawer.Navigator
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Landing"
+        component={Landing}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="WorkOut"
+        component={WorkOut}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="FitScreen"
+        component={FitScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Rest"
+        component={Rest}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AdminStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MyAdmin"
+        component={Admin}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="AdminOrders"
+        component={AdminOrders}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="AdminProducts"
+        component={Sub}
+        options={{headerShown: false}}
+      />
+      {/* <Stack.Screen name="Admin" component={Admin}  options={{headerShown: false}} />
+           <Stack.Screen name="ChangePassword" component={ChangePassword}  options={{headerShown: false}} /> */}
+    </Stack.Navigator>
+  );
+};
+
+const ProfileStack = ({navigation}) => {
+  const routes = useNavigationState(state => state.routes);
+
+  useEffect(() => {
+    const currentRouteIndex =
+      routes?.length && routes[routes.length - 1].state?.index;
+
+    const currentRoute = currentRouteIndex
+      ? routes[routes.length - 1].state?.routeNames[currentRouteIndex]
+      : '';
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      // Reset the stack to "MyProfile" if its not my profile.
+      if (currentRoute !== 'MyProfile') {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'MyProfile'}],
+          }),
+        );
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation, routes]);
+
+  return (
+    <Stack.Navigator initialRouteName="MyProfile">
+      <Stack.Screen
+        name="MyProfile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Orders"
+        component={Orders}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Sub" component={Sub} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Admin"
+        component={AdminStack}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ChangePassword"
+        component={ChangePassword}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ProductStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MyProduct"
+        component={Product}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Cart"
+        component={Cart}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Checkout"
+        component={Checkout}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Pay" component={Pay} options={{headerShown: false}} />
+    </Stack.Navigator>
+  );
+};
+
+const Main = ({navigation}) => {
+  // const Drawer = createNativeDrawerNavigator();
+  // const { isOnBoardingDisabled } = useSelector((state) => state.user);
+
+  return (
+    <Drawer.Navigator
       initialRouteName={'Home'}
       screenOptions={{
-        headerShown:false,
-        drawerLabelStyle:{marginLeft:-25,
-          fontFamily:'Roboto-Medium',
-          fontSize:15
+        headerShown: false,
+        drawerLabelStyle: {
+          marginLeft: -25,
+          fontFamily: 'Roboto-Medium',
+          fontSize: 15,
         },
         // drawerActiveBackgroundColor:'red',
-        drawerActiveTintColor:'#333',
-        drawerInactiveTintColor:'#333'
+        drawerActiveTintColor: '#333',
+        drawerInactiveTintColor: '#333',
       }}
-      drawerContent={props=><CustomDrawer {...props}/>}
-      >
-            
-              <Drawer.Screen name="Home" component={HomeStack} options={{
+      drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          drawerIcon: color => <HomeIcon size={22} color={'black'} />,
+        }}
+      />
 
-                drawerIcon:(color)=>(
-                  <HomeIcon size={22} color={'black'}/>
-                )
-              }} />
-             
-             
-            
-              <Drawer.Screen name="Product" component={ProductStack}   options={{ drawerIcon:(color)=>(
-                  <ShoppingCartIcon size={22} color={'#333'}/>
-                )}} />
-                <Drawer.Screen name="Profile" component={ProfileStack}  options={{ drawerIcon:(color)=>(
-                  <UserIcon size={22} color={'black'}/>
-                )}} />
-             
- 
- </Drawer.Navigator>
-  )
-}
+      <Drawer.Screen
+        name="Product"
+        component={ProductStack}
+        options={{
+          drawerIcon: color => <ShoppingCartIcon size={22} color={'#333'} />,
+        }}
+      />
+
+      <Drawer.Screen
+        name="Profile"
+        component={ProfileStack}
+        options={{
+          drawerIcon: color => <UserIcon size={22} color={'black'} />,
+          onPress: () => console.log('see'),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default Main;
